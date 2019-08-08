fe03acd502c3de497e0419e3be995ddeae7b29a1
umitkose <umiitkose@gmail.com>
2019-08-08 23:11:42 +0300

    HelloWorld paketi eklendi

---
+++ app/src/main/java/com/example/kotlinexamples/datatype/DataType.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinexamples.datatype
+
+fun main(args: Array<String>){
+    val value="Merhaba"  // Val can not be reassigned
+    // value = "Merhaba Dünya"  --> Error
+
+
+    var variableName : String ="Merhaba" //var can be reassigned
+    variableName = "Merhaba Dünya"
+
+    var variableName1 = 33
+    // variableName1= "333"  Error : Type mismatch.
+
+
+}
