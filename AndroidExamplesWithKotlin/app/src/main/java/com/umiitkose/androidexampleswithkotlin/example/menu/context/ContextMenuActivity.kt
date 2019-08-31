253b650862a422c59bb9a53637725b90fbe63c2e
Ümit <umiitkose@gmail.com>
2019-08-31 18:03:54 +0300

    "Material Design ve Fragment çalışılıyor."

--- AndroidExamplesWithKotlin/app/src/main/java/com/umiitkose/androidexampleswithkotlin/example/menu/context/ContextMenuActivity.kt
+++ AndroidExamplesWithKotlin/app/src/main/java/com/umiitkose/androidexampleswithkotlin/example/menu/context/ContextMenuActivity.kt
@@ -33,6 +33,14 @@ class ContextMenuActivity : AppCompatActivity() {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu_context, menu)
         menu?.setHeaderTitle("Seçenekler")
+
+        /* menu_context xml 'i oluşturmadan tasarlamak için :
+        *
+        menu?.setHeaderTitle("Context Menu")
+        menu?.add(0, v?.id!!, 0, "Call")
+        menu?.add(0, v?.id!!, 1, "SMS")
+        menu?.add(1, v?.id!!, 0, "Search")
+        * */
     }
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
@@ -49,6 +57,25 @@ class ContextMenuActivity : AppCompatActivity() {
 
             else -> super.onContextItemSelected(item)
         }
+
+        /*  title 'dan kontrol için :
+
+         when {
+            item?.title == "Call" -> {
+                Toast.makeText(applicationContext, "Call", Toast.LENGTH_LONG).show()
+                return true
+            }
+            item?.title == "SMS" -> {
+                Toast.makeText(applicationContext, "SMS", Toast.LENGTH_LONG).show()
+                return true
+            }
+            item?.title == "Search" -> {
+                Toast.makeText(applicationContext, "Search", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> return super.onContextItemSelected(item)
+        }
+        */
     }
 
 }
