da59b3f280386464e454d7797169f1ee210fec27
Ümit <umiitkose@gmail.com>
2019-09-14 23:44:26 +0300

    "Recycler View Eklendi"

--- AndroidExamplesWithKotlin/app/src/main/java/com/umiitkose/androidexampleswithkotlin/example/sharedprefences/SharedPreferencesActivity.kt
+++ AndroidExamplesWithKotlin/app/src/main/java/com/umiitkose/androidexampleswithkotlin/example/sharedprefences/SharedPreferencesActivity.kt
@@ -5,15 +5,19 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.umiitkose.androidexampleswithkotlin.R
+import kotlinx.android.synthetic.main.activity_explicit_intent.*
 import kotlinx.android.synthetic.main.activity_shared_preferences.*
+import java.util.prefs.Preferences
 
 class SharedPreferencesActivity : AppCompatActivity() {
 
     private lateinit var sharedPref: SharedPreferences
+    private lateinit var prefs: SharedPreferences
 
     companion object{
         val PREFERENCE_FILE_KEY = "my_pref_key"
         val EDT_KEY = "edk_key"
+        val CHB_KEY = "chb_key"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +25,24 @@ class SharedPreferencesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_shared_preferences)
         // Başka uygulamaların erişimini kapatmak için MODE_PRIVATE yaptık. Yalnızca bu uygulamadan okuyacağız. İlk parametre de xml dosya yolu
         sharedPref = getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE)
+        prefs = getPreferences(Context.MODE_PRIVATE)
 
+        readValues()
+
+        btn_spclick.setOnClickListener {
+            saveChbValue()
+            saveEdtValue()
+        }
+
+    }
+
+    private fun saveChbValue() {
+        val chbEditor: SharedPreferences.Editor = prefs.edit()
+
+        chbEditor.apply(){
+            putBoolean(CHB_KEY,chb_kontrol.isChecked)
+            apply()
+        }
     }
 
     private fun saveEdtValue(){
@@ -35,6 +56,10 @@ class SharedPreferencesActivity : AppCompatActivity() {
 
     private fun readValues(){
         val value = sharedPref.getString(EDT_KEY,"none")
-        
+        textView.text = value
+
+
+        val chbValue = prefs.getBoolean(CHB_KEY,false)
+        chb_kontrol.isChecked = chbValue
     }
 }
